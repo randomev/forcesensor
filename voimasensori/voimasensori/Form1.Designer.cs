8823338ac3a21d0db35e0dff8d2daf31575862a9
Vaingald <vaino.palonen@icloud.com>
2019-05-11 12:19:36 +0300

    First working version, reads sensor and draws graph.

---
+++ voimasensori/voimasensori/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace voimasensori
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.lblLoad1 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.warningIcon = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(543, 188);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // lblLoad1
+            // 
+            this.lblLoad1.AutoSize = true;
+            this.lblLoad1.Location = new System.Drawing.Point(65, 228);
+            this.lblLoad1.Name = "lblLoad1";
+            this.lblLoad1.Size = new System.Drawing.Size(35, 13);
+            this.lblLoad1.TabIndex = 1;
+            this.lblLoad1.Text = "label1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.warningIcon});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // warningIcon
+            // 
+            this.warningIcon.Name = "warningIcon";
+            this.warningIcon.Size = new System.Drawing.Size(12, 17);
+            this.warningIcon.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.lblLoad1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label lblLoad1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel warningIcon;
+    }
+}
+
---
+++ voimasensori/voimasensori/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Phidget22;
+using Phidget22.Events;
+
+namespace voimasensori
+{
+    public partial class Form1 : Form
+    {
+        VoltageRatioInput ratio = null; //declare our voltageratio object that will be used to connect to the bridge device
+        double m = 41809117.297, b = 2412.078; //our data points as well as the calculated slope and intercept
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ratio.Close(); //clean up to prevent issues down the line
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ratio = new VoltageRatioInput(); //create an instance of the object
+
+            //register the pertinent event handlers
+            ratio.Attach += ratio_attach;
+            ratio.Detach += ratio_detach;
+            ratio.VoltageRatioChange += ratio_change;
+
+            try
+            {
+                ratio.Open(); //open the device
+            }
+            catch (PhidgetException ex)
+            {//let user know if there's an error while opening
+                warningIcon.Text = "Error connecting to device: " + ex.Description;
+                warningIcon.Visible = true;
+            }
+
+        }
+
+        void ratio_attach(object sender, Phidget22.Events.AttachEventArgs e)
+        {
+            VoltageRatioInput attached = (VoltageRatioInput)sender;
+        }
+
+        void ratio_detach(object sender, Phidget22.Events.DetachEventArgs e)
+        {
+            //reset form when device detaches
+            warningIcon.Text = "Detached";
+            ratio.Channel = 0;
+        }
+
+        void ratio_change(object sender, Phidget22.Events.VoltageRatioInputVoltageRatioChangeEventArgs e)
+        {
+            double l = ((m * e.VoltageRatio) + b);
+            lblLoad1.Text = l.ToString("f3"); //if we have input the necessary information we can output a load value as data comes in
+
+            addToGraph(l, chart1);
+        }
+
+        void addToGraph(double value, System.Windows.Forms.DataVisualization.Charting.Chart chart)
+        {
+            chart.Series.First().Points.Add(value);
+            
+        }
+
+    }
+}
